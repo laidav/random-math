12bae5fdf33d073b17e32b846ac93dda811a5ceb
David Lai <laidav@gmail.com>
2021-04-21 15:22:14 -0400

    minor refactor

--- src/app/models/problem.ts
+++ src/app/models/problem.ts
@@ -4,21 +4,19 @@ export class Problem {
 
   top: number;
   bottom: number;
-  answer: number;
+  answer?: number;
   operation: Operation;
 
   constructor(operation: Operation) {
     this.operation = operation;
 
-    switch (operation) {
-      case Operation.Subtraction:
-        this.top = this.getRandomInt(this.MAX, 10);
-        this.bottom = this.getRandomInt(this.top);
-        break;
-      default:
-        this.top = this.getRandomInt(this.MAX);
-        this.bottom = this.getRandomInt(this.MAX);
-    };
+    if(operation === Operation.Subtraction) {
+      this.top = this.getRandomInt(this.MAX, 10);
+      this.bottom = this.getRandomInt(this.top);
+    } else {
+      this.top = this.getRandomInt(this.MAX);
+      this.bottom = this.getRandomInt(this.MAX);
+    }
 
     switch (operation) {
       case Operation.Addition:
@@ -29,6 +27,8 @@ export class Problem {
         break;
       case Operation.Multiplication:
         this.answer = this.top * this.bottom;
+        break;
+      default:
         break;
     };
   }
